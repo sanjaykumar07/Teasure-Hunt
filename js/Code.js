6857cdb0a486d38a7668ca09919b5934d89bf639
Sanjay Kumar <73998919+sanjaykumar07@users.noreply.github.com>
2021-04-15 17:07:21 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    fill("lightblue")
+    
+    text("R E V B A I L A", 100,50)
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+    text("A T A D E S B A", 700,150)
+    text("Hint: Use for multiplayer game !!", 700,170)
+    
+    text("C U T N I F O N", 200,280)
+    text("Hint: Use for a better presentation !!", 200,300)
+
+}
